f3697c12d615f48442b60093cc88db8457ffa0d2
Joel-Almeid <joelabreumartins@gmail.com>
2026-06-02 01:55:23 -0300

    feat: build estatico finalizado e css corrigido

--- site_advgilson1-main/src/main.tsx
+++ site_advgilson1-main/src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routes/routeTree.gen'
-import './routes/styles.css'
+import { routeTree } from './routeTree.gen'
+import './styles.css'
 
 const router = createRouter({ routeTree })
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
