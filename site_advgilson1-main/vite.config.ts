898273f9f7c1461fb6c3afbffb65dcd687d90223
Joel-Almeid <joelabreumartins@gmail.com>
2026-06-02 01:54:11 -0300

    feat: build estatico finalizado e css corrigido

--- site_advgilson1-main/vite.config.ts
+++ site_advgilson1-main/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
